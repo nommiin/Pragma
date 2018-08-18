283e306a6080604dc78e450134d976fdcf5393f8
torin! <torinfrei@gmail.com>
2018-08-18 14:45:08 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -16,12 +16,19 @@ Pragma = {
 	},
 	Runner: {
 		Pointer: 0,
+		Error: function(error) {
+			console.error(`A runner error has occured at position ${Pragma.Runner.Pointer}:\n${error}`);
+		},
 		Process: function(praBytecode) {
 			let praTimer = new Date();
 			while (Pragma.Runner.Pointer < praBytecode.length) {
 				switch (praBytecode[Pragma.Runner.Pointer]) {
 					case 0: { // push
-						Pragma.Runner.Stack.push(praBytecode[++Pragma.Runner.Pointer]);
+						if (praBytecode[++Pragma.Runner.Pointer] == 2) {
+							Pragma.Runner.Stack.push(Pragma.Runner.GetVariable(praBytecode[++Pragma.Runner.Pointer]));
+						} else {
+							Pragma.Runner.Stack.push(praBytecode[++Pragma.Runner.Pointer]);
+						}
 						break;
 					}
 
@@ -40,6 +47,27 @@ Pragma = {
 						for(let i = 0, _i = Pragma.Runner.Stack.pop(); i < _i; i++) {
 							praArguments.push(Pragma.Runner.Stack.pop());
 						}
+						Pragma.Runner.Functions[praFunction](praArguments);
+						break;
+					}
+
+					case 4: { // add
+						Pragma.Runner.Stack.push(Pragma.Runner.Stack.pop() + Pragma.Runner.Stack.pop());
+						break;
+					}
+
+					case 5: { // sub
+						Pragma.Runner.Stack.push(Pragma.Runner.Stack.pop() - Pragma.Runner.Stack.pop());
+						break;
+					}
+
+					case 6: { // div
+						Pragma.Runner.Stack.push(Pragma.Runner.Stack.pop() / Pragma.Runner.Stack.pop());
+						break;
+					}
+
+					case 7: { // mul
+						Pragma.Runner.Stack.push(Pragma.Runner.Stack.pop() * Pragma.Runner.Stack.pop());
 						break;
 					}
 				}
@@ -50,7 +78,26 @@ Pragma = {
 					break;
 				}
 			}
-			console.log(`Bytecode was successfully ran in ${new Date().getTime() - praTimer}ms`);
+			console.log(`Bytecode was successfully ran in ${new Date().getTime() - praTimer}ms}`);
+			console.log(Pragma.Runner.Local)
+		},
+		GetVariable: function(name) {
+			for(var praVariable in Pragma.Runner.Local) {
+				if (praVariable == name) {
+					return Pragma.Runner.Local[praVariable];
+				}
+			}
+			for(var praVariable in Pragma.Runner.Global) {
+				if (praVariable == name) {
+					return Pragma.Runner.Global[praVariable];
+				}
+			}
+			Pragma.Runner.Error(`Unable to find variable with name: ${name}`);
+		},
+		Functions: {
+			"print": function(argument) {
+				console.log(argument[0]);
+			}
 		},
 		Stack: [],
 		Local: {},
@@ -113,7 +160,41 @@ Pragma = {
 				"push": 0,
 				"locl": 1,
 				"glob": 2,
-				"call": 3
+				"call": 3,
+				"add": 4,
+				"sub": 5,
+				"div": 6,
+				"mul": 7
+			}
+		},
+		Types: {
+			Get: function(v) {
+				let praInput = (typeof v == "string" ? v.trim() : v);
+				if (typeof praInput == "string") {
+					if (praInput[0] == "\"" && praInput[praInput.length - 1] == "\"") {
+						return Pragma.Runtime.Types.Find("string")
+					} else {
+						for(var i = 0; i < praInput.length; i++) {
+							if (praInput.charCodeAt(i) < 48 || praInput.charCodeAt(i) > 57) {
+								return Pragma.Runtime.Types.Find("variable");
+							}
+						}
+						return Pragma.Runtime.Types.Find("number");
+					}
+				}
+				return Pragma.Runtime.Types.Find("number");
+			},
+			Find: function(n) {
+				if (n in Pragma.Runtime.Types.List == false) {
+					Pragma.Compiler.Warning(`Could not find unsupported type: ${n}`);
+					return -1;
+				}
+				return Pragma.Runtime.Types.List[n];
+			},
+			List: {
+				"number": 0,
+				"string": 1,
+				"variable": 2
 			}
 		},
 		Bytecode: []
@@ -160,9 +241,7 @@ Pragma = {
 			let praChars = (typeof find == "object" ? find : [find]);
 			for(var i = Pragma.Compiler.Index; i < Pragma.Compiler.Input.length; i++) {
 				for(var praChar in praChars) {
-					console.log(Pragma.Compiler.Input.slice(i, i + praChar.length) == praChar);
-					if (Pragma.Compiler.Input.slice(i, i + praChar.length) == praChar) {
-						console.log("found it!");
+					if (Pragma.Compiler.Input.slice(i, i + praChars[praChar].length) == praChars[praChar]) {
 						return i;
 					}
 				}
@@ -180,49 +259,87 @@ Pragma = {
 			return _array;
 		},
 		Tokens: {
-			"var": function() {
-				// TODO: Fix this!!!
+			"local": function() {
 				let praAssignment = Pragma.Compiler.FindAt("="),
 					praName = Pragma.Compiler.Input.slice(Pragma.Compiler.MoveTo("="), Pragma.Compiler.Index++).trim(),
 					praValue = Pragma.Compiler.Input.slice(Pragma.Compiler.MoveTo([";", ","]), Pragma.Compiler.Index).trim();
-				/*switch (""){//Pragma.Compiler.Input[praAssignment - 1]) {
+				
+				switch (Pragma.Compiler.Input[praAssignment - 1]) {
 					case "+": {
-						console.log(Pragma.Runtime.Locals.List)
+						praName = praName.slice(0, praName.length - 1).trim();
 						if (praName in Pragma.Runtime.Locals.List) {
-							console.log("add")
+							Pragma.Runtime.Locals.List[praName] += Pragma.Util.Convert(praValue);
+							Pragma.Runtime.Locals.Add(praName, praValue);
+							Pragma.Runtime.Instructions.Add("push", [Pragma.Runtime.Types.List["variable"], praName]);
+							Pragma.Runtime.Instructions.Add("push", [Pragma.Runtime.Types.Get(praValue), praValue]);
+							Pragma.Runtime.Instructions.Add("add");
 						} else {
-							Pragma.Compiler.Error(`Could not add to uninitalized variable.`);
+							Pragma.Compiler.Error(`Could not add to uninitalized variable: ${praName}`);
 						}
 						break;
 					}
 
 					case "-": {
+						praName = praName.slice(0, praName.length - 1).trim();
+						if (praName in Pragma.Runtime.Locals.List) {
+							Pragma.Runtime.Locals.List[praName] += Pragma.Util.Convert(praValue);
+							Pragma.Runtime.Locals.Add(praName, praValue);
+							Pragma.Runtime.Instructions.Add("push", [Pragma.Runtime.Types.List["variable"], praName]);
+							Pragma.Runtime.Instructions.Add("push", [Pragma.Runtime.Types.Get(praValue), praValue]);
+							Pragma.Runtime.Instructions.Add("sub");
+						} else {
+							Pragma.Compiler.Error(`Could not subtract from uninitalized variable: ${praName}`);
+						}
 						break;
 					}
 
-					case "/=": {
+					case "/": {
+						praName = praName.slice(0, praName.length - 1).trim();
+						if (praName in Pragma.Runtime.Locals.List) {
+							Pragma.Runtime.Locals.List[praName] += Pragma.Util.Convert(praValue);
+							Pragma.Runtime.Locals.Add(praName, praValue);
+							Pragma.Runtime.Instructions.Add("push", [Pragma.Runtime.Types.List["variable"], praName]);
+							Pragma.Runtime.Instructions.Add("push", [Pragma.Runtime.Types.Get(praValue), praValue]);
+							Pragma.Runtime.Instructions.Add("div");
+						} else {
+							Pragma.Compiler.Error(`Could not divide by uninitalized variable: ${praName}`);
+						}
 						break;
 					}
 
-					case "*=": {
+					case "*": {
+						praName = praName.slice(0, praName.length - 1).trim();
+						if (praName in Pragma.Runtime.Locals.List) {
+							Pragma.Runtime.Locals.List[praName] += Pragma.Util.Convert(praValue);
+							Pragma.Runtime.Locals.Add(praName, praValue);
+							Pragma.Runtime.Instructions.Add("push", [Pragma.Runtime.Types.List["variable"], praName]);
+							Pragma.Runtime.Instructions.Add("push", [Pragma.Runtime.Types.Get(praValue), praValue]);
+							Pragma.Runtime.Instructions.Add("mul");
+						} else {
+							Pragma.Compiler.Error(`Could not multiply by uninitalized variable: ${praName}`);
+						}
 						break;
 					}
-				}*/
-				
+
+					default: {
+						Pragma.Runtime.Instructions.Add("push", [Pragma.Runtime.Types.Get(praValue), praValue]);
+					}
+				}
+
 				Pragma.Runtime.Locals.Add(praName, praValue);
-				Pragma.Runtime.Instructions.Add("push", [praValue]);
 				Pragma.Runtime.Instructions.Add("locl", [praName]);
+
 				if (Pragma.Compiler.Input[Pragma.Compiler.Index] == ",") {
 					Pragma.Compiler.Index++;
-					this.var();
+					this.local();
 				}
 			},
-			"globalvar": function() {
+			"global": function() {
 				let praName = Pragma.Compiler.Input.slice(Pragma.Compiler.MoveTo("="), Pragma.Compiler.Index++).trim(),
 					praValue = Pragma.Compiler.Input.slice(Pragma.Compiler.MoveTo([";", ","]), Pragma.Compiler.Index).trim();
-				console.log(`globalvar = [Name: ${praName}, Value: ${praValue}]`);
+				console.log(`global = [Name: ${praName}, Value: ${praValue}]`);
 				Pragma.Runtime.Locals.Add(praName, praValue);
-				Pragma.Runtime.Instructions.Add("push", [praValue]);
+				Pragma.Runtime.Instructions.Add("push", [Pragma.Runtime.Types.Get(praValue), praValue]);
 				Pragma.Runtime.Instructions.Add("glob", [praName]);
 				if (Pragma.Compiler.Input[Pragma.Compiler.Index] == ",") {
 					Pragma.Compiler.Index++;
@@ -244,6 +361,13 @@ Pragma = {
 		Pragma.Compiler.Index = 0;
 		Pragma.Compiler.Lines = [];
 		Pragma.Compiler.Line = 0;
+		// Load in built functions
+		for(var praFunction in Pragma.Runner.Functions) {
+			Pragma.Runtime.Functions.Add(praFunction, []);
+			console.log(Pragma.Runtime.Functions.List[Pragma.Runtime.Functions.List.length - 1]);
+			console.log(Pragma.Runner.Functions);
+		}
+
 		// Get line numbers
 		for(Pragma.Compiler.Index = 0; Pragma.Compiler.Index < Pragma.Compiler.Input.length; Pragma.Compiler.Index++) {
 			if (Pragma.Compiler.Input[Pragma.Compiler.Index].charCodeAt(0) == 10) {
@@ -279,10 +403,32 @@ Pragma = {
 							}
 
 							for(var j = praArguments.length - 1; j >= 0; j--) {
-								Pragma.Runtime.Instructions.Add("push", [praArguments[j]]);
+								Pragma.Runtime.Instructions.Add("push", [Pragma.Runtime.Types.Get(praArguments[j]), praArguments[j]]);
 							}
-							Pragma.Runtime.Instructions.Add("push", [praArguments.length]);
+							Pragma.Runtime.Instructions.Add("push", [Pragma.Runtime.Types.List["number"], praArguments.length]);
 							Pragma.Runtime.Instructions.Add("call", [praFunction.Name]);
+							praFound = true;
+							break;
+						}
+					}
+				}
+
+				// Local Variables
+				if (praFound == false) {
+					for(var praVariable in Pragma.Runtime.Locals.List) {
+						if (Pragma.Compiler.Input.slice(Pragma.Compiler.Index, Pragma.Compiler.Index + praVariable.length) == praVariable) {
+							Pragma.Compiler.Tokens["local"]();
+							praFound = true;
+							break;
+						}
+					}
+				}
+
+				// Global Variables
+				if (praFound == false) {
+					for(var praVariable in Pragma.Runtime.Globals.List) {
+						if (Pragma.Compiler.Input.slice(Pragma.Compiler.Index, Pragma.Compiler.Index + praVariable.length) == praVariable) {
+							Pragma.Compiler.Tokens["global"]();
 							praFound = true;
 							break;
 						}
@@ -293,6 +439,7 @@ Pragma = {
 			}
 		}
 		console.log(`Compile has finished at ${((new Date()).getHours()%13).toString().padStart(2, "0")}:${((new Date()).getMinutes()).toString().padStart(2, "0")}:${((new Date()).getSeconds()).toString().padStart(2, "0")} ${(new Date()).getHours()>12?"PM":"AM"} in ${(new Date()).getTime() - Pragma.Compiler.Start}ms`);
+		Pragma.Runner.Process(Pragma.Runtime.Bytecode);
 	}
 }
 /*Pragma = {
